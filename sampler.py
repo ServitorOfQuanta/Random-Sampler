e7bcc2321fb9a065b6a76bd6f99037f9918c073c
ServitorOfQuanta <56535216+ServitorOfQuanta@users.noreply.github.com>
2023-11-06 00:33:33 +0000

    Add comments and restrictions

--- sampler.py
+++ sampler.py
@@ -4,16 +4,30 @@ import argparse
 
 
 def sample(pool_m: Sequence, n: int):
+    """This function performs a random sample selection of size n from a pool (pool_m) without replacement.
+
+    Args:
+        pool_m (Sequence): Any sequence of objects that we select from.
+        n (int): Any non-negative integer lower than the length of pool_m. This represents the number of draws that will be made.
+
+    Raises:
+        AssertionError: This function will throw an assertion error if n is found to be negative or higher than the length of pool_m
+
+    Returns:
+        list: _description_
+    """
     length = len(pool_m)
     samples = []
+
     if n > length or n < 0:
         raise AssertionError(
             "The number of samples requested either exceeds the number of the samples in the pool or is negative."
         )
     elif n == length:
-        return pool_m
+        return list(pool_m)
     elif n == 0:
         return []
+
     places = list(range(length))
     for _ in range(n):
         max_place = len(places)
@@ -33,5 +47,8 @@ if __name__ == "__main__":
     parser.add_argument("--n", type=int, help="Number of samples requested")
 
     input_args = parser.parse_args()
+
+    if not (input_args.m or input_args.n):
+        parser.error("No pool or number given, please call with both --m and --n")
 
     print(sample(input_args.m, input_args.n))
